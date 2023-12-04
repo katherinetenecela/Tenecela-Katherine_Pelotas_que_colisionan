21143076e577c62a09c2369e22ced2e41231eb0b
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2023-12-04 14:04:46 -0500

    2 círculos

--- sketch.js
+++ sketch.js
@@ -5,3 +5,18 @@ function setup() {
 function draw() {
   circle(mouseX, mouseY, 20);
 }
+let x1 = mouseX;
+let y1 = mouseY;
+let radio1 = 40;
+
+let x2 = width / 2;
+let y2 = height / 2;
+let radio2 = 60;
+
+let distancia = dist(x1, y1, x2, y2);
+
+ellipse(x1, y1, 2 * radio1);
+ellipse(x2, y2, 2 * radio2);
+
+stroke(0);
+line(x1, y1, x2, y2);
