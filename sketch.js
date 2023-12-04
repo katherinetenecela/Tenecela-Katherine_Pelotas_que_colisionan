7817ffd3ca0d245836e3067e7ab097ff00888144
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2023-12-04 15:07:47 -0500

    Círculos colisionan

--- sketch.js
+++ sketch.js
@@ -3,20 +3,27 @@ function setup() {
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  background(220);
+
+  let x1 = mouseX;
+  let y1 = mouseY;
+  let radio1 = 40;
+
+  let x2 = width / 2;
+  let y2 = height / 2;
+  let radio2 = 60;
+
+  let distancia = dist(x1, y1, x2, y2);
+
+  if (distancia < radio1 + radio2) {
+    fill(255, 0, 0);
+  } else {
+    fill(0, 0, 255);
+  }
+
+  ellipse(x1, y1, 2 * radio1);
+  ellipse(x2, y2, 2 * radio2);
+
+  stroke(0);
+  line(x1, y1, x2, y2);
 }
-let x1 = mouseX;
-let y1 = mouseY;
-let radio1 = 40;
-
-let x2 = width / 2;
-let y2 = height / 2;
-let radio2 = 60;
-
-let distancia = dist(x1, y1, x2, y2);
-
-ellipse(x1, y1, 2 * radio1);
-ellipse(x2, y2, 2 * radio2);
-
-stroke(0);
-line(x1, y1, x2, y2);
