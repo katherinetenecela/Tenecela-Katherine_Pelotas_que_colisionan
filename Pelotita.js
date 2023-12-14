6a14845c066c3fa0056fb052329cf0e0ff797c01
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2023-12-14 14:10:37 -0500

    Pelotitas

--- Pelotita.js
+++ Pelotita.js
@@ -1,15 +1,44 @@
-class Pelotita 
-constructor(){
-    this.pos =createVector(random(width), random (heihgt));
-    this.vel =createVector(random(10), 0);
-    this.vel= rotate(random(tWO_PI));
+class Pelotita {
+  constructor() {
+    this.pos = createVector(random(width), random(heihgt));
+    this.vel = createVector(random(5), 0);
+    this.vel = rotate(random(tWO_PI));
+    this.c = color(300, 150, 30);
 
-}
-update(){
-    this.pos.add(this.vel)
+    this.diam = int(random(10, 100));
+  }
+  update() {
+    this.pos.add(this.vel);
 
-}
+    if (this.pos.x > width - 10) {
+      this.vel.x *= -1;
+    }
+    if (this.pos.x < 0 + 10) {
+      this.vel.x *= -1;
+    }
 
-display(){
-    circle(this.pos.x, this.pos.y, 20);
+    if (this.pos.y > height - 10) {
+      this.vel.y *= -1;
+    }
+    if (this.pos.y < 0 + 10) {
+      this.vel.y *= -1;
+    }
+  }
+
+  display() {
+    fill(this.c);
+    circle(this.pos.x, this.pos.y, this.diam);
+  }
+
+  colorRamdon() {
+    this.c = color(random(20), random(255), random(255));
+  }
+
+  colision(_px) {
+    this.distancia = p5.Vector.dist(this.pos, _px.pos);
+    if (this.distancia < this.diam / 2 + _px.diam / 2) {
+      this.colorRandom();
+      this.vel.rotate(PI);
+    }
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,12 +1,27 @@
+let p = [];
+let np = 10;
+
 function setup() {
 createCanvas(windowWidth, windowHeight);
-p =new Pelotita();
+for (leti =0; i< np; i++){
+  let newP = new Pelotita();
+  p.push(newP);
+}
 }
 
 
 function draw() {
-  p.update();
-  p.display()
+  background(255)
+for (let i = 0; i <np; i++){
+  p[i].update();
+  p[i].display();
+
+  for(let iP = 0; iP < np; iP++){
+    p[i].display(p[iP]);
+  }
+}
+
+  
   
 }
 
